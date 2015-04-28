81c1d1dfa291eefb7dcaec199598ed336828efc2
mao8 <thisisma08@gmail.com>
2015-04-29 03:42:48 +0530

    i3 Added bark.sh

---
+++ i3/bark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+notify-send "Locking the screen. Woof"
+paplay /usr/share/sounds/gnome/default/alerts/drip.ogg
+paplay /usr/share/sounds/gnome/default/alerts/drip.ogg
+paplay /usr/share/sounds/gnome/default/alerts/drip.ogg
+paplay /usr/share/sounds/gnome/default/alerts/drip.ogg
+paplay /usr/share/sounds/gnome/default/alerts/drip.ogg
+paplay /usr/share/sounds/gnome/default/alerts/drip.ogg
+paplay /usr/share/sounds/gnome/default/alerts/bark.ogg
