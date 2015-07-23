10337068de672198d534567892b2cb530aa80ba7
mao8 <thisisma08@gmail.com>
2015-07-23 18:45:53 +0530

    Changed font of urxvt

--- i3/ram.sh
+++ i3/ram.sh
@@ -2,7 +2,7 @@
 while [ "true" ]
 do
   used="$(free -m | sed '3q;d' | sed 's/.*cache:\s*\([0-9]*\)\s.*/\1/')"
-  cmp="$(echo "$used > 2900" | bc)" 
+  cmp="$(echo "$used > 5000" | bc)" 
   #echo $cmp
   if [[ "$cmp" == "1" ]]; then
     #echo "aa"
